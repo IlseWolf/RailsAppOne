fc4b821e88c497c58cc64c5a16bc97a0bd40fba2
IlseWolf <i.e.m.wolf@gmail.com>
2016-01-14 17:55:30 +0100

    added price column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113183900) do
+ActiveRecord::Schema.define(version: 20160114164233) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160113183900) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.string   "latin_name"
+    t.integer  "price"
   end
 
 end
