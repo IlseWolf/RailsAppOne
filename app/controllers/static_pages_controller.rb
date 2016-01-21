a4e0c30834fba24015f2a7490fb5b88e8abdd8d7
IlseWolf <i.e.m.wolf@gmail.com>
2016-01-21 21:20:48 +0100

    added carousel @featurd_product and @product still failty

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
   end 
+
   def landing_page
   	@featured_product = Product.first 
   end
-end
+
+  def landing_page
+    @products = Product.limit(3)
+  end
+end 
