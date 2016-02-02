9eb10f436a97ae7bfdd4c5bf7c8743f06052bb89
IlseWolf <i.e.m.wolf@gmail.com>
2016-02-02 20:05:17 +0100

    typo in email adres

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'i.e.mw.wolf@gmail.com.com',
+        :to => 'i.e.m.wolf@gmail.com.com',
         :subject => "A new contact form message from #{name}")
   end
 
