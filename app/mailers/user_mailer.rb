e3d65229cecd1b118359e32dfef96bf823b217d6
IlseWolf <i.e.m.wolf@gmail.com>
2016-02-02 20:07:35 +0100

    email wrong again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'i.e.m.wolf@gmail.com.com',
+        :to => 'i.e.m.wolf@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
 
