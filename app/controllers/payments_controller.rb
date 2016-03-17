52ce2c165a8ca006a8463da907ffc62372d15396
IlseWolf <i.e.m.wolf@gmail.com>
2016-03-17 18:55:50 +0100

    fixed payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,36 +1,31 @@
+
 class PaymentsController < ApplicationController
 
   def create
-    @product = Product.find(params[:product_id])
-    @user = current_user 
     token = params[:stripeToken]
-    # Create the charge on Stripe's servers - this will charge the user's card
-      begin
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    # Create the charge on Stripe's servers = this will charge the user's card
+    
+    begin
       charge = Stripe::Charge.create(
-        :amount => (@product.price*100)
+        :amount => (@product.price*100).to_i, # amount in cents
         :currency => "usd",
         :source => token,
         :description => params[:stripeEmail]
-      )
-    
-    if charge.paid 
-      Order.create(
-        :user => @user.id,
-        :product => @product.id,
-        :total => @product.price
-      )
+        )
+
+      if charge.paid
+        Order.create(product_id: @product, user_id: @user, total: @product.price)
+      end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your 
+      payment: #{err[:message]}"
     end
- 
-
-	  rescue Stripe::CardError => e
-	    # The card has been declined
-	    body = e.json_body
-	    err = body[:error]
-	    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-	  end
-	  redirect_to product_path(product)
+    redirect_to product_path(@product)
   end
-
-  redirect_to product_path(@product), notice: "Thank you for you order. Hoot!"
-  end  
-end
+end 
