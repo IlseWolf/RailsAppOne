e4c6fbceda934b6fa83e8c33e0a3c93d8d50d7e0
IlseWolf <i.e.m.wolf@gmail.com>
2016-03-28 14:55:34 +0200

    added ajax to comment section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
 	      if @comment.save
 	        format.html { redirect_to @product, notice: 'Review was created successfully.' }
 	        format.json { render :show, status: :created, location: @product }
+	        fromat.js #for ajax request to work 
 	      else
 	        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
 	        format.json { render json: @comment.errors, status: :unprocessable_entity }
