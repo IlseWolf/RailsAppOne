aa71d80d234473478d471e85a8ed18362ddac752
IlseWolf <i.e.m.wolf@gmail.com>
2016-03-30 20:34:38 +0200

    extra save for caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["23914a"],
+                     :password => ENV["5b271712987b591cd1226894db5a42b2"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
